19faaf048624e208f8662f3adb0bda75e8846f02
Phenomenium <74776897+Phenomenium@users.noreply.github.com>
2022-05-17 13:47:23 +0200

    Ajout Algorithme 2

---
+++ script/utils.js
@@ -0,0 +1,28 @@
+// Normaliser le texte
+// return {string} texte normalisé
+const normalizeText = (text) => {
+  return text
+    .toLowerCase()
+    .normalize("NFD") // décomposer les graphèmes combinés en simple
+    .replace(/[\u0300-\u036f]/g, ""); //enlever les signes diacritiques
+};
+
+// Créer les élements du DOM avec la class
+const createGenericElt = (eltTag, eltClass = null) => {
+  const elt = document.createElement(eltTag);
+  if (eltClass) elt.className = eltClass;
+  return elt;
+};
+
+// Créer les lien des élements avec la class
+const createLinkElt = (eltHref, eltContent, eltClass = null) => {
+  const elt = createGenericElt("a", eltClass);
+  elt.setAttribute("href", eltHref);
+  elt.setAttribute("title", eltContent);
+  elt.textContent = eltContent;
+
+  return elt;
+};
+
+export { createGenericElt, createLinkElt, normalizeText };
+
